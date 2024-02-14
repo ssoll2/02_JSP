ad43d8b221d3daf02065bf637b01c74c2e131c6a
ssoll2 <150099188+ssoll2@users.noreply.github.com>
2024-02-14 20:25:20 +0900

    Create InsertOkServlet.java

---
+++ 07emp/InsertOkServlet.java
@@ -0,0 +1,82 @@
+package com.emp.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.emp.model.EmpDAO;
+import com.emp.model.EmpDTO;
+
+@WebServlet("/insert_ok.do")
+public class InsertOkServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+   
+    public InsertOkServlet() {
+        super();
+    }
+
+	
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 요청과 응답 시 한글 깨짐 방지 설정 작업.
+				request.setCharacterEncoding("UTF-8");
+				response.setContentType("text/html; charset=UTF-8");
+				
+				// 1단계 : emp_insert.jsp 페이지에서 넘어온 데이터들을 받아주어야 한다.
+				int emp_no = Integer.parseInt(request.getParameter("num").trim());
+				
+				String emp_name = request.getParameter("name").trim();
+				
+				String emp_job = request.getParameter("job").trim();
+				
+				int emp_mgr = Integer.parseInt(request.getParameter("mgr").trim());
+				
+				int emp_sal = Integer.parseInt(request.getParameter("sal").trim());
+				
+				int emp_comm = Integer.parseInt(request.getParameter("comm").trim());
+				
+				int emp_deptno = Integer.parseInt(request.getParameter("deptno").trim());
+				
+				// 2단계 : 넘어온 데이터들을 DB에 전송해 주어야 한다.
+				//        전송 시 DTO 객체를 이용한다.
+				EmpDTO dto = new EmpDTO();
+				
+				dto.setEmpno(emp_no);
+				dto.setEname(emp_name);
+				dto.setJob(emp_job);
+				dto.setMgr(emp_mgr);
+				dto.setSal(emp_sal);
+				dto.setComm(emp_comm);
+				dto.setDeptno(emp_deptno);
+				
+				EmpDAO dao = EmpDAO.getInstance();
+				
+				int check = dao.insertEmp(dto);
+				
+				PrintWriter out = response.getWriter();
+				
+				if(check > 0) {
+					out.println("<script>");
+					out.println("alert('사원 등록 성공!!!')");
+					out.println("location.href='select.do'");
+					out.println("</script>");
+				}else {
+					out.println("<script>");
+					out.println("alert('사원 등록 실패~~~')");
+					out.println("history.back()");
+					out.println("</script>");
+				}
+				
+				
+			
+
+			
+	
+	
+	}
+
+}

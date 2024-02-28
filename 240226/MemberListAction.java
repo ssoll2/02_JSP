b06f5ac313f08d514f9aa9a09eef8f5a8434e6bb
ssoll2 <150099188+ssoll2@users.noreply.github.com>
2024-02-28 11:35:59 +0900

    feat: 12_Member_MVC2

---
+++ 240226/MemberListAction.java
@@ -0,0 +1,34 @@
+package com.member.action;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.member.model.MemberDAO;
+import com.member.model.MemberDTO;
+
+public class MemberListAction implements Action {
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) {
+		
+		// 요청 : 전체 회원 목록을 보여달라고 요청.
+		// 응답 : member10 테이블에 있는 회원 전체 리스트를
+		//  	 조회하여 view page로 이동시키는 비지니스 로직
+		MemberDAO dao = MemberDAO.getInstance();
+		
+		List<MemberDTO> list = dao.getMemberList();
+		
+		// DB에서 가져온 전체 회원 리스트를 
+		// view page로 이동시켜야 함.
+		request.setAttribute("List", list);
+		
+		return "member/member_list.jsp";
+		
+		
+	
+
+	}
+
+}

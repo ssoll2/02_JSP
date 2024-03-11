9f6a4e2b1fc034e761b39bdb46c20bdd9b3fa505
ssoll2 <150099188+ssoll2@users.noreply.github.com>
2024-03-11 16:54:45 +0900

    Create Action.java

---
+++ 14_board_reply/Action.java
@@ -0,0 +1,16 @@
+package com.reply.action;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public interface Action {
+	
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws IOException;
+	
+		
+	
+	
+
+}
